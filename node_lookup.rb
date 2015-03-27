864113f18ee19822070a3d533b473eefbe350be8
d_dcrow <d_dcrow@outlook.com>
2015-03-28 01:52:01 +0300

    Finished debbuging node_lookup.rb. Allfound bugs fixed. Working properly. Now needs proper formatting. Also a method to visualy represent the path. And to work with other root_nodes.

--- node_lookup.rb
+++ node_lookup.rb
@@ -11,6 +11,7 @@ class Node
 		@curr_node = start_node
 		@nodeStack = Array.new
 		@nodePath = Array.new
+		@nodePath.push start_node
 		@wentBack = false
 		if num_cools == start_node
 			(1..(data_array.length)/num_cools).each do |i|
@@ -29,50 +30,116 @@ class Node
  	@nodePath = Array.new # Путь который мы использовали чтобы попасть в @curr_node
 	@nodeStack = Array.new # Вершины от которых мы еще не посетили второй соседней вершины
 	@wentBack = false # Для знания вернулись ли в вершину из которой не посетили второго соседа или нет.
-
+	@pathEnd = false
 	# Проверить есть ли соседняя вершина
 	def check_forward(n)
 		c = @curr_node + n
-		if @data_hash.has_key?(c)
-			show_value(c,"Key")
-			return true
+		if n - 1 != 1
+			if @data_hash.has_key?(c)
+				show_value(c,"Key")
+				return true
+			else
+				show_value(c,"key")
+				return false
+			end
 		else
-			show_value(c,"key")
 			return false
 		end
 	end
 
 	# Перейти в соседнюю вершину
-	def go_forward(n)
+	def go_left()
 		show_value(@curr_node,"curr_node")
-		@nodeStack.push @curr_node
+		if check_forward(@num_cools + 1)
+			@nodeStack.push @curr_node
+		end
+		go(@num_cools)
+	end
+
+	def go(n)
 		@curr_node += n
 		@nodeSum += @data_hash[@curr_node]
 		@nodePath.push @curr_node
 		@wentBack = false
+	end
+
+	def go_right()
+		show_value(@curr_node,"curr_node")
+		go(@num_cools + 1)
 	end 
 
  	# Вернуться в вершину от которой мы не посещали вторую соседнюю вершину
 	def go_back()
-		if @nodeStack.empty? #ТУТ ПРОБЛЕМА. НАДО ИСПРАВИТЬ.
-			@wentBack = true
+		# if @nodeStack.empty? #ТУТ ПРОБЛЕМА. НАДО ИСПРАВИТЬ.
+		# 	@wentBack = true
+		# else
+		# 	@wentBack = false
+		# 	return
+		# end
+		@wentBack = true
+		show_value(@nodePath, "Current Node Path s")
+		show_value(@nodeSum, "Current Node Sum s")
+		show_value(@nodeStack,"Current Node Stack s")
+		show_value(@curr_node, "Current Node s")
+		if !@nodeStack.empty?
+
+			back_node = @nodeStack.pop
+			
+			
+			i_r = (@curr_node - back_node - 1)/@num_cools - 1
+			show_value(i_r, "i_r")
+			i_l = (@curr_node - back_node)/@num_cools - 1
+			show_value(i_l,"i_l")
+
+			if (i_r.is_a? Integer) && i_r != 0 && i_r > 0
+				(0..i_r).each do |x|
+					b_nod = @nodePath.pop
+					show_value(@nodePath, "nodePath")
+					show_value(b_nod, "b_nod")
+					show_value(@data_hash[b_nod], "data_hash")
+					@nodeSum -= @data_hash[b_nod]
+					
+				end
+			elsif (i_l.is_a? Integer) && i_l != 0 && i_l > 0
+				(0..i_l).each do |x|
+					b_nod = @nodePath.pop
+					show_value(@nodePath, "nodePath")
+					show_value(b_nod, "b_nod")
+					show_value(@data_hash[b_nod], "data_hash")
+					@nodeSum -= @data_hash[b_nod]
+					
+				end
+			else
+				b_nod = @nodePath.pop
+				@nodeSum -= @data_hash[@curr_node]
+			end
+			show_value(back_node,"Popped value")
+			show_value(@nodePath, "Current Node Path")
+			show_value(@nodeSum, "Current Node Sum")
+			show_value(@nodeStack,"Current Node Stack")
+			@curr_node = back_node
+			show_value(@curr_node, "Current Node")
 		else
-			@wentBack = false
-			return
+			@pathEnd = true
 		end
-		back_node = @nodeStack.pop
-		show_value(back_node,"Popped value")
-		@nodeSum -= data_array[curr_node]
-		@nodePath.pop @curr_node
-		@curr_node = back_node
 	end
 
 	# Проверить не является ли сумма на ныншнем пути наибольшей
 	def checkMaxSum()
+		show_value(@@maxNodeSum,"maxNodeSum")
+		show_value(@nodeSum,"nodeSum")
 		maxSum = [@nodeSum,@@maxNodeSum].max
-		if @maxNodeSum != maxSum
-			@@nodeMaxPath = @nodePath
-			@maxNodeSum = maxSum
+		show_value(maxSum, "maxSum")
+		if maxSum == @nodeSum
+			(0..@@nodeMaxPath.length).each do |i|
+				@@nodeMaxPath.pop
+			end
+			(1..@nodePath.length).each do |i|
+				@@nodeMaxPath.push @nodePath[i-1]
+			end
+			#@@nodeMaxPath = @nodePath
+			@@maxNodeSum = maxSum
+			show_value(@@nodeMaxPath,"nodeMaxPath f")
 		end
 	end
 
@@ -84,23 +151,21 @@ class Node
 			#show_value_bool(@wentBack, "wentBack")
 			if (check_forward(left) && (@wentBack == false))
 				print_mess("Went Forward Left")
-				show_value(@curr_node, "curr_node")
-				go_forward(left)
+				#show_value(@curr_node, "curr_node")
+				go_left()
 				
 			elsif check_forward(right)
 				print_mess("Went Forward Right")
 				show_value(@curr_node, "curr_node")
-				go_forward(right)
+				go_right()
 				
-			elsif @wentBack == false
+			else
 				checkMaxSum()
 				print_mess("wentBack")
 				go_back()
-				
-			else
-				@wentBack == true
 			end
-		end while !@nodeStack.empty? && @wentBack == false
+
+		end while !@pathEnd
 	end
 
 	#For TESTING PURPOSES
@@ -110,6 +175,30 @@ class Node
 
 	def getWentBack()
 		return @wentBack
+	end
+
+	def getNodeSum()
+		return @nodeSum
+	end
+
+	def getNodePath()
+		return @nodePath
+	end
+
+	def getNodeStack()
+		return @nodeStack
+	end
+
+	def getPathEnd()
+		return @pathEnd
+	end
+
+	def getMaxSum()
+		return @@maxNodeSum
+	end
+
+	def getMaxPath()
+		return @@nodeMaxPath
 	end
 
 	def makeNumberArray(n)
--- node_lookup_test.rb
+++ node_lookup_test.rb
@@ -3,13 +3,39 @@ require_relative "method_tester.rb"
 require_relative "path_finder.rb"
 
 # Load data
-data_ar = parse_data_array(read_file("test_file4.txt"))
+data_ar = parse_data_array(read_file("test_file3.txt"))
 data_ar_m = parse_data(read_file("test_file2.txt"))
 #show_value(data_ar,"test_file2")
 puts data_ar
-colls = numOfCollumns(read_file("test_file4.txt"))
+colls = numOfCollumns(read_file("test_file3.txt"))
 show_value(colls, "Collumns")
 
+def testF(node1, colls)
+	left = colls
+	right = colls + 1
+	i = 0
+	begin
+		print_mess("Try #{i}")
+		if (node1.check_forward(left) && (node1.getWentBack() == false))
+					print_mess("Went Forward Left")
+					#show_value(@curr_node, "curr_node")
+					node1.go_left()
+					
+				elsif node1.check_forward(right)
+					print_mess("Went Forward Right")
+					#show_value(@curr_node, "curr_node")
+					node1.go_right()
+					
+				else
+					node1.checkMaxSum()
+					print_mess("wentBack")
+					node1.go_back()
+				end
+				i += 1
+				show_value(node1.getMaxPath(),"nodeMaxPath")
+				show_value(node1.getNodePath().length, "node path length")
+	end while !node1.getPathEnd()
+end
 
 test_start(1)
 
@@ -27,4 +53,13 @@ test_start(1)
 	#show_value((node1.check_forward(5) && (node1.getWentBack() != true)),"eq")
 	node1.findMaxSumPath()
 	#puts Node.maxNodeSum
+	
+
+	#node1.checkMaxSum()
+	puts node1.getMaxSum()
+	puts node1.getMaxPath()
+	#puts node1.getNodeSum()
+	#puts node1.getNodePath()
+
+
 test_end(1)
