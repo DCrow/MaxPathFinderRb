6e953cf05360f11b7d531300d8433b6ccd67266b
d_dcrow <d_dcrow@outlook.com>
2015-03-31 19:10:42 +0300

    Fully finished tree searcher

--- node_lookup-test_version.rb
+++ node_lookup-test_version.rb
@@ -1,21 +1,23 @@
+require_relative "path_finder.rb"
+require_relative "method_tester.rb"
 class Node
 
 	# Конструктор. Принимает массив данных. Количество столбцов в таблице данных
 	# из которой сделан массив. И начальную вершину первой строки.
-	def initialize(data_array, num_cools)
+	def initialize(data_array, num_cools, start_node)
 		@data_hash = Hash[makeNumberArray(data_array.length - 1).zip(data_array)]
 		@num_cools = num_cools
-		@nodeSum = data_array[0]
-		@curr_node = 0
-		@start_node = 0
+		@nodeSum = data_array[start_node]
+		@curr_node = start_node
+		@start_node = start_node
 		@nodeStack = Array.new
 		@nodePath = Array.new
-		@nodePath.push 0
+		@nodePath.push start_node
 		@wentBack = false
 	end
 
-private
-	@start_node # Значение первоначальной вершины в древе
+	public
+	@start_node = 0
 	@data_hash # Для хранения значений
 	@num_cools = 0 # Хранение значений столбцов в таблице
 	@nodeSum = 0 # Сумма вершин предшествующих нынешней с учетом нынешней
@@ -26,41 +28,42 @@ private
 	@nodeStack = Array.new # Вершины от которых мы еще не посетили второй соседней вершины
 	@wentBack = false # Для знания вернулись ли в вершину из которой не посетили второго соседа или нет.
 	@pathEnd = false
-
-	# Вспомогательный массив чисел от 0 до n для создания хеша данных
-	def makeNumberArray(n)
-		num_arr = Array.new
-		(0..n).each do |k|
-			num_arr[k] = k
-		end
-		return num_arr
-	end
-
 	# Проверить есть ли соседняя вершина
 	def check_forward(n)
 		c = @curr_node + n
-
 			if @data_hash.has_key?(c) && (n - @start_node - 2 != 0)
+				show_value(c,"Key")
 				return true
 			else
+				show_value(c,"key")
 				return false
 			end
 	end
 
-	# Переход из вершины в соседнюю вершину
-	def go(n)
+	# Перейти в соседнюю вершину
+	def go_left()
+		show_value(@curr_node,"curr_node")
+		if check_forward(@num_cools + 1)
+			@nodeStack.push @curr_node
+		end
+		go(@num_cools)
+	end
 
+	def go(n)
 		if n == @num_cools && check_forward(@num_cools + 1)
 			@nodeStack.push @curr_node
 		end
-
 		@curr_node += n
 		@nodeSum += @data_hash[@curr_node]
 		@nodePath.push @curr_node
 		@wentBack = false
 	end
 
-	# Обновляет все переменные для прохода следующего древа с новым рутом
+	def go_right()
+		show_value(@curr_node,"curr_node")
+		go(@num_cools + 1)
+	end 
+
 	def renew(n)
 		@start_node += n
 		@curr_node = @start_node
@@ -74,41 +77,68 @@ private
 
  	# Вернуться в вершину от которой мы не посещали вторую соседнюю вершину
 	def go_back()
+		# if @nodeStack.empty? #ТУТ ПРОБЛЕМА. НАДО ИСПРАВИТЬ.
+		# 	@wentBack = true
+		# else
+		# 	@wentBack = false
+		# 	return
+		# end
+		
+
+		
+		show_value(@nodePath, "Current Node Path s")
+		show_value(@nodeSum, "Current Node Sum s")
+		show_value(@nodeStack,"Current Node Stack s")
+		show_value(@curr_node, "Current Node s")
 		if !@nodeStack.empty?
 
 			@wentBack = true
 			back_node = @nodeStack.pop
 			
 			i_r = (@curr_node - back_node - 1)/@num_cools - 1
+			show_value(i_r, "i_r")
 			i_l = (@curr_node - back_node)/@num_cools - 1
+			show_value(i_l,"i_l")
 
 			if (i_r.is_a? Integer) && i_r != 0 && i_r > 0
 				(0..i_r).each do |x|
 					b_nod = @nodePath.pop
+					show_value(@nodePath, "nodePath")
+					show_value(b_nod, "b_nod")
+					show_value(@data_hash[b_nod], "data_hash")
 					@nodeSum -= @data_hash[b_nod]
+					
 				end
 			elsif (i_l.is_a? Integer) && i_l != 0 && i_l > 0
 				(0..i_l).each do |x|
 					b_nod = @nodePath.pop
+					show_value(@nodePath, "nodePath")
+					show_value(b_nod, "b_nod")
+					show_value(@data_hash[b_nod], "data_hash")
 					@nodeSum -= @data_hash[b_nod]
+					
 				end
 			else
 				b_nod = @nodePath.pop
 				@nodeSum -= @data_hash[@curr_node]
 			end
-
+			show_value(back_node,"Popped value")
+			show_value(@nodePath, "Current Node Path")
+			show_value(@nodeSum, "Current Node Sum")
+			show_value(@nodeStack,"Current Node Stack")
 			@curr_node = back_node
+			show_value(@curr_node, "Current Node")
 		else
-
 			@pathEnd = true
 		end
 	end
 
 	# Проверить не является ли сумма на ныншнем пути наибольшей
 	def checkMaxSum()
-
+		show_value(@@maxNodeSum,"maxNodeSum")
+		show_value(@nodeSum,"nodeSum")
 		maxSum = [@nodeSum,@@maxNodeSum].max
-
+		show_value(maxSum, "maxSum")
 		if maxSum == @nodeSum
 			(0..@@nodeMaxPath.length).each do |i|
 				@@nodeMaxPath.pop
@@ -116,44 +146,82 @@ private
 			(1..@nodePath.length).each do |i|
 				@@nodeMaxPath.push @nodePath[i-1]
 			end
-
+			#@@nodeMaxPath = @nodePath
 			@@maxNodeSum = maxSum
+			show_value(@@nodeMaxPath,"nodeMaxPath f")
 		end
 	end
-
-public
 
 	# Поиск пути наибольшей суммы
 	def findMaxSumPath()
-
 		left = @num_cools
 		right = @num_cools + 1
-
 		(1..@num_cools-@start_node).each do |i|
-			begin
+			show_value(@start_node, "start_node")
+		begin
+			#show_value_bool(@wentBack, "wentBack")
+			if (check_forward(left) && (@wentBack == false))
+				print_mess("Went Forward Left")
+				#show_value(@curr_node, "curr_node")
+				go(left)
+				
+			elsif check_forward(right)
+				print_mess("Went Forward Right")
+				show_value(@curr_node, "curr_node")
+				go(right)
+				
+			else
+				checkMaxSum()
+				print_mess("wentBack")
+				go_back()
+			end
 
-				if (check_forward(left) && (@wentBack == false))
-					go(left)
-				elsif check_forward(right)
-					go(right)	
-				else
-					checkMaxSum()
-					go_back()
-				end
-			end while !@pathEnd
-
-			renew(1)
-		end
+		end while !@pathEnd
+		renew(1)
+	end
+		puts @@nodeMaxPath
+		puts @@maxNodeSum
 	end
 
-	# Возвращает путь максимальной суммы
-	def getNodeMaxPath()
+	#For TESTING PURPOSES
+	def getDataHash()
+		return @data_hash
+	end
+
+	def getWentBack()
+		return @wentBack
+	end
+
+	def getNodeSum()
+		return @nodeSum
+	end
+
+	def getNodePath()
+		return @nodePath
+	end
+
+	def getNodeStack()
+		return @nodeStack
+	end
+
+	def getPathEnd()
+		return @pathEnd
+	end
+
+	def getMaxSum()
+		return @@maxNodeSum
+	end
+
+	def getMaxPath()
 		return @@nodeMaxPath
 	end
 
-	# Возвращает максимальную сумму
-	def getNodeMaxSum()
-		return @@maxNodeSum
+	def makeNumberArray(n)
+		num_arr = Array.new
+		(0..n).each do |k|
+			num_arr[k] = k
+		end
+		return num_arr
 	end
 
 end
--- node_lookup_test.rb
+++ node_lookup_test.rb
@@ -1,4 +1,4 @@
-require_relative "node_lookup.rb"
+require_relative "node_max_path_searcher"
 require_relative "method_tester.rb"
 require_relative "path_finder.rb"
 
@@ -40,13 +40,13 @@ end
 test_start(1)
 
 	#node1 = Node.new(data_ar, colls, 0)
-	node2 = Node.new(data_ar, colls, 7)
-	puts node2.getDataHash()
-	if node2.getDataHash().has_key?(15)
-		puts "Yes"
-	else
-		puts "no"
-	end
+	node2 = Node.new(data_ar, colls)
+	# puts node2.getDataHash()
+	# if node2.getDataHash().has_key?(15)
+	# 	puts "Yes"
+	# else
+	# 	puts "no"
+	# end
 	#show_value_bool(node1.check_forward(5),"check_forward")
 	#show_value_bool(node1.getWentBack(),"went back")
 	#show_value_bool((node1.getWentBack() == false),"getWentBack")
@@ -54,12 +54,14 @@ test_start(1)
 	#show_value((node1.check_forward(5) && (node1.getWentBack() != true)),"eq")
 	#node1.findMaxSumPath()
 	node2.findMaxSumPath()
+	puts node2.getNodeMaxPath()
+	puts node2.getNodeMaxSum()
 	#puts Node.maxNodeSum
 	
 
 	#node1.checkMaxSum()
-	puts node2.getMaxSum()
-	puts node2.getMaxPath()
+	#puts node2.getMaxSum()
+	#puts node2.getMaxPath()
 	#puts node1.getNodeSum()
 	#puts node1.getNodePath()
 
